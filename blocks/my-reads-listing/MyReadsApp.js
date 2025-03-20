b1e17ef0769846debc41c09c2a6319a181182d0b
Nate Finch <n8finch@gmail.com>
2025-03-19 21:09:23 -0500

    filters almost work

--- blocks/my-reads-listing/MyReadsApp.js
+++ blocks/my-reads-listing/MyReadsApp.js
@@ -2,105 +2,116 @@ import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import PostList from './components/PostList';
 import PostListByYear from './components/PostListByYear';
+import GenreButtons from './components/GenreButtons';
+import SearchForm from './components/SearchForm';
 
-const SearchForm = ( { search, setSearch } ) => {
-	return (
-		<form>
-			{ /* <label htmlFor='search'>Search:</label> */ }
-			<input
-				type="text"
-				id="search"
-				placeholder={ __( 'Search reads', 'my-reads' ) }
-				value={ search }
-				onChange={ ( event ) => setSearch( event.target.value ) }
-			/>
-		</form>
-	);
-};
+const MyReadsFilterApp = ({ attributes }) => {
+  const { layout, order, useAmazonLink } = attributes;
+  // postsArray is the array of all posts without the years as the key.
+  const [postsArray, setPostsArray] = useState([]);
+  // postsJSON is the array of all posts with the years as the key.
+  const [postsJSON, setPostsJSON] = useState([]);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [genreFilter, setGenreFilter] = useState('All');
 
-const MyReadsFilterApp = ( { attributes } ) => {
-	const { layout, order, useAmazonLink } = attributes;
-	// postsArray is the array of all posts without the years as the key.
-	const [ postsArray, setPostsArray ] = useState( [] );
-	// postsJSON is the array of all posts with the years as the key.
-	const [ postsJSON, setPostsJSON ] = useState( [] );
-	const [ search, setSearch ] = useState( '' );
-	const [ loading, setLoading ] = useState( true );
+  // const allReadsEndpoint = '/wp-json/my-reads/v1/all-the-reads';
+  const allReadsEndpoint = '/wp-content/uploads/all-the-reads.json';
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  };
 
-	// const allReadsEndpoint = '/wp-json/my-reads/v1/all-the-reads';
-	const allReadsEndpoint = '/wp-content/uploads/all-the-reads.json';
-	const requestOptions = {
-		method: 'GET',
-		headers: { 'Content-Type': 'application/json' },
-	};
+  useEffect(() => {
+    fetch(allReadsEndpoint, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setPostsJSON(data);
+        const allPosts = Object.values(data).flat();
+        setPostsArray(allPosts);
+        setLoading(false);
+      })
+      .catch((error) => console.error('Error fetching posts:', error));
+  }, []);
 
-	useEffect( () => {
-		fetch( allReadsEndpoint, requestOptions )
-			.then( ( response ) => response.json() )
-			.then( ( data ) => {
-				setPostsJSON( data );
-				const allPosts = Object.values( data ).flat();
-				setPostsArray( allPosts );
-				setLoading( false );
-			} )
-			.catch( ( error ) =>
-				console.error( 'Error fetching posts:', error )
-			);
-	}, [] );
+  const getFilteredPosts = () => {
+    return postsArray.filter(
+      (post) =>
+        // if the post title or categories include the search term
+        post.title.toLowerCase().includes(search.toLowerCase()) ||
+        post.genres.some((category) =>
+          category.toLowerCase().includes(search.toLowerCase())
+        ) ||
+        post.year.toLowerCase().includes(search.toLowerCase()) ||
+        post.excerpt.toLowerCase().includes(search.toLowerCase()) ||
+        post._my_reads_format.toLowerCase().includes(search.toLowerCase()) ||
+        (post._my_reads_isFavorite && 'Favorites' === genreFilter)
+    );
+  };
 
-	return (
-		<>
-			<SearchForm search={ search } setSearch={ setSearch } />
-			{ loading && (
-				<p>
-					<em>Loading posts...</em>
-				</p>
-			) }
-			{ '' !== search ? (
-				<PostList
-					posts={ postsArray.filter(
-						( post ) =>
-							// if the post title or categories include the search term
-							post.title
-								.toLowerCase()
-								.includes( search.toLowerCase() ) ||
-							post.genres.some( ( category ) =>
-								category
-									.toLowerCase()
-									.includes( search.toLowerCase() )
-							) ||
-							post.year
-								.toLowerCase()
-								.includes( search.toLowerCase() ) ||
-							post.excerpt
-								.toLowerCase()
-								.includes( search.toLowerCase() ) ||
-							post._my_reads_format
-								.toLowerCase()
-								.includes( search.toLowerCase() )
-					) }
-				/>
-			) : (
-				<>
-					{ 'list' === layout && (
-						<PostList
-							posts={ postsArray }
-							layout={ layout }
-							useAmazonLink={ useAmazonLink }
-						/>
-					) }
-					{ 'row' === layout && (
-						<PostListByYear
-							posts={ postsJSON }
-							order={ order }
-							layout={ layout }
-							useAmazonLink={ useAmazonLink }
-						/>
-					) }
-				</>
-			) }
-		</>
-	);
+  // This callback will update your filtering logic based on the active genre.
+  const handleFilterChange = (selectedGenre) => {
+    setGenreFilter(selectedGenre);
+    // You can adjust your filtering here (for instance, if 'Favorites', filter posts by favorites,
+    // or if a specific genre, filter posts whose `genres` include that genre)
+    if (selectedGenre === 'All') {
+      setPostsArray(Object.values(postsJSON).flat()); // Reset to all posts
+    } else if (selectedGenre === 'Favorites') {
+      setPostsArray(postsArray.filter((post) => post._my_reads_isFavorite));
+    } else {
+      setPostsArray(
+        postsArray.filter((post) => post.genres.includes(selectedGenre))
+      );
+    }
+  };
+
+  return (
+    <>
+      <GenreButtons
+        posts={getFilteredPosts()}
+        onFilterChange={handleFilterChange}
+      />
+      <SearchForm
+        search={search}
+        setSearch={setSearch}
+        postsArray={postsArray}
+      />
+      {search && (
+        <p>
+          {__(
+            `Showing ${getFilteredPosts().length} results for "${search}"`,
+            'my-reads'
+          )}
+        </p>
+      )}
+      {loading && (
+        <p>
+          <em>Loading posts...</em>
+        </p>
+      )}
+      {'' !== search || 'All' !== genreFilter ? (
+        <PostList posts={getFilteredPosts()} />
+      ) : (
+        <>
+          {'list' === layout && (
+            <PostList
+              posts={getFilteredPosts()}
+              layout={layout}
+              useAmazonLink={useAmazonLink}
+            />
+          )}
+          {'row' === layout && (
+            <PostListByYear
+              posts={postsJSON}
+              order={order}
+              layout={layout}
+              useAmazonLink={useAmazonLink}
+            />
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default MyReadsFilterApp;
---
+++ blocks/my-reads-listing/components/SearchForm.js
@@ -0,0 +1,18 @@
+import { __ } from '@wordpress/i18n';
+
+const SearchForm = ({ search, setSearch, postsArray }) => {
+  return (
+    <form>
+      {/* <label htmlFor='search'>Search:</label> */}
+      <input
+        type='text'
+        id='search'
+        placeholder={__(`Search all ${postsArray.length} reads...`, 'my-reads')}
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchForm;
