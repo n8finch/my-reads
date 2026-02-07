069be2685ea0d483dd67b27ed286047e04a1d553
Nate Finch <n8finch@gmail.com>
2026-02-07 10:40:21 -0600

    cleanup author block

--- blocks/my-reads-author/edit.js
+++ blocks/my-reads-author/edit.js
@@ -2,34 +2,53 @@ import { __ } from '@wordpress/i18n';
 import { useEffect } from '@wordpress/element';
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 import { useEntityProp } from '@wordpress/core-data';
+import { post } from '@wordpress/icons';
 
-export default function Edit( { context: { postType, postId } } ) {
-	const [ meta, setMeta ] = useEntityProp(
-		'postType',
-		postType,
-		'meta',
-		postId
-	);
+export default function Edit({ context: { postType, postId } }) {
+  const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId);
 
-	const onChangeAuthor = ( val ) => {
-		setMeta( {
-			...meta,
-			_myreads_author: val,
-		} );
-	};
+  useEffect(() => {
+    if (postType !== 'myreads') {
+      return;
+    }
+    setMeta({
+      ...meta,
+      _myreads_author: meta?._myreads_author ? meta?._myreads_author : '',
+    });
+  }, []);
 
-	return (
-		<div { ...useBlockProps() }>
-			<p>
-				<span>{ __( ' Author: ', 'my-reads' ) }</span>
-				<RichText
-					tagName="span"
-					value={ meta._myreads_author }
-					allowedFormats={ [] }
-					onChange={ onChangeAuthor }
-					placeholder={ __( 'Author...' ) }
-				/>
-			</p>
-		</div>
-	);
+  const onChangeAuthor = (val) => {
+    if (postType !== 'myreads') {
+      return;
+    }
+
+    setMeta({
+      ...meta,
+      _myreads_author: val,
+    });
+  };
+
+  return (
+    <div {...useBlockProps()}>
+      {'myreads' === postType ? (
+        <p>
+          <span>{__(' Author: ', 'my-reads')}</span>
+          <RichText
+            tagName='span'
+            value={meta?._myreads_author ?? ''}
+            allowedFormats={[]}
+            onChange={onChangeAuthor}
+            placeholder={__('Author...')}
+          />
+        </p>
+      ) : (
+        <p>
+          {__(
+            'This block is only interactive in My Reads posts, otherwise it is a placeholder.',
+            'my-reads',
+          )}
+        </p>
+      )}
+    </div>
+  );
 }
